a9a887936244428a2b90993f61f0ded51c655646
subhajit100 <asubhajit35@gmail.com>
2025-04-12 18:27:13 +0530

    initial setup of user-journal microservices application

---
+++ api-gateway/src/test/java/com/subhajit/microservices/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subhajit.microservices.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ journal-service/src/test/java/com/subhajit/microservices/journal_service/JournalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subhajit.microservices.journal_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JournalServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
